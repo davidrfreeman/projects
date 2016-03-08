55624000ad869a5b56e2be7ea7fbb99196b2c2ce
David Freeman <davidfreeman@holos.solutions>
2016-03-08 14:28:38 -0700

    completed assignment 8.4

--- test.py
+++ test.py
@@ -2,6 +2,13 @@ fname = raw_input("Enter file name: ")
 fh = open(fname)
 lst = list()
 for line in fh:
-    line = line.rstrip()
+	# this creates a list of each line
     line = line.split()
-    print line
+    # second loop to go over each word in lists
+    for i in line:
+    	# checks to see if each word is unique
+    	# and if so adds it to the originaly empty array
+    	if i not in lst:
+    		lst.append(i)
+lst.sort()
+print lst
