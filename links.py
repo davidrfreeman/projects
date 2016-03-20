ea16671c626a13a8c1b7a47024fefeefe6cd81c4
David Freeman <davidfreeman@holos.solutions>
2016-03-19 19:00:42 -0600

    completed assignment

--- links.py
+++ links.py
@@ -1,32 +1,20 @@
-# Note - this code must run in Python 2.x and you must download
-# http://www.pythonlearn.com/code/BeautifulSoup.py
-# Into the same folder as this program
+# Note - this code must run in Python 2.x 
 
 import urllib
 from bs4 import *
 
-url = raw_input('Enter: ')
+url = raw_input('Enter URL: ')
 count = int(raw_input('Enter count: '))
 pos = int(raw_input('Enter position: ')) - 1
 
 html = urllib.urlopen(url).read()
 soup = BeautifulSoup(html, "html.parser")
 
-links = []
-
-# Retrieve all of the anchor tags
-tags = soup('a')
-
 print "Retrieving: ", url
 
 for i in range(count):
-	# loop fills list with all links on page
-	for tag in tags:
-		links.append(tag.get('href', None))
-	html = urllib.urlopen(links[pos]).read()
+	tags = soup('a')
+	url = tags[pos].get('href', None)
+	html = urllib.urlopen(url).read()
 	soup = BeautifulSoup(html, "html.parser")
-	print "Retrieving: ", links[pos]
-	# clears out list to be filled again by for loop
-	links = links[:]
-	# removes 1 from count so that the loop will eventually end
-	count -= 1
+	print "Retrieving: ", tags[pos].get('href', None)
